deeb9b3a231b0132a5c131cc56b28f20886865ed
UwayoOlga <uwayoolga@gmail.com>
2025-07-07 16:52:51 +0200

    function that sqaures a number

---
+++ pyhonlearning/square_number.py
@@ -0,0 +1,7 @@
+#Write a function called square(number) that returns the square of a number. Call the function with the number 5 and print the result.
+
+def square(number):
+    return number * number
+
+result = square(5)
+print(result)
