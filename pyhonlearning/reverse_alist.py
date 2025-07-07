98c44e70df888b482ad045a00c493f1c5f15012d
UwayoOlga <uwayoolga@gmail.com>
2025-07-07 17:38:31 +0200

    Reversing a list and finding the maximum

---
+++ pyhonlearning/reverse_alist.py
@@ -0,0 +1,10 @@
+#Given the list numbers = [4, 7, 2, 9, 5], write code to:
+# ▪Print the list in reverse order.
+# ▪Find and print the largest number.
+
+numbers = [4, 7, 2, 9, 5]
+ 
+print("Reversed list:", numbers[::-1])
+ 
+largest_number = max(numbers)
+print("Largest number:", largest_number)
